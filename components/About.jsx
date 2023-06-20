86c8144dcad3a9f9ce426363987f3f5cb0d0c010
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 05:18:45 -0700

    moved AboutReact to /components/About.jsx

---
+++ components/About.jsx
@@ -0,0 +1,15 @@
+export default function AboutReact() {
+    return (
+        <section>        
+            <h2>Fun facts about React</h2>
+    
+            <ul className="factoids">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </section>
+    )
+}
--- index.jsx
+++ index.jsx
@@ -1,21 +1,6 @@
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
-
-function AboutReact() {
-    return (
-        <section>        
-            <h2>Fun facts about React</h2>
-    
-            <ul className="factoids">
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100k stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
-        </section>
-    )
-}
+import AboutReact from "./components/About"
 
 function LearnReact() {
     return (
