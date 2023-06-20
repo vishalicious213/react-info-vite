4f9a6dd8e5f7b98aa69c7aab4cad60ba9a557e86
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 05:20:37 -0700

    moved Footer to /components/Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <small>© 2023 React In Development. All rights reserved.</small>
+        </footer>
+    )
+}
--- index.jsx
+++ index.jsx
@@ -2,14 +2,7 @@ import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import AboutReact from "./components/About"
 import LearnReact from "./components/Learn"
-
-function Footer() {
-    return (
-        <footer>
-            <small>© 2023 React In Development. All rights reserved.</small>
-        </footer>
-    )
-}
+import Footer from "./components/Footer"
 
 function App() {
     return (
