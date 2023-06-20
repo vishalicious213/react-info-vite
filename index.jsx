e379857a01691097a7d358d5a5d9d5e8f310c891
vishalicious213 <vishalicious213@gmail.com>
2023-06-20 05:19:37 -0700

    moved LearnReact to /components/Learn.jsx

--- index.jsx
+++ index.jsx
@@ -1,25 +1,7 @@
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import AboutReact from "./components/About"
-
-function LearnReact() {
-    return (
-        <section>
-            <h2>Why I want to learn React</h2>
-
-            <ul className="factoids">
-                <li>To build web applications</li>
-                <li>To get a high-paying web developer job</li>
-                <ul>
-                    <li>To save my family and house</li>
-                    <li>To fuel development of my own company and help other junior developers</li>
-                    <li>To build the front-end of a new clinical application dream</li>
-                </ul>
-                <li>To help combat job and financial insecurity</li>
-            </ul>
-        </section>
-    )
-}
+import LearnReact from "./components/Learn"
 
 function Footer() {
     return (
